e365164e8fd9c306b25e796eacb13f437e2e5a0d
kradam <lovidmann@gmail.com>
2022-09-27 17:55:09 +0200

    igy?

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 
+TODO_API_URL = "https://jsonplaceholder.cypress.io/todos"
 
-
-def parser(url):
+def fetch_todos():
     response = requests.get(url)        
     parsed = response.json()
-    print(parsed[0])
 
-parser("https://jsonplaceholder.cypress.io/todos")
+fetch_todos(TODO_API_URL)
+
+if __name__ == "__main__":
+    print(r.headers())
